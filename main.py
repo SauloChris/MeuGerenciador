2c6e1bb8d7492135b116965766e8f681221384b0
Saulo Christian Alves Machado <saulo.machado@academico.ifrn.edu.br>
2022-07-14 20:10:25 -0300

    Criação de atributos

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
+import datetime
 
 filename = 'texto1.txt'
-
-print("Tamanho = ",os.path.getsize(filename))
-print("Data da ultima modificação = ",os.path.getmtime(filename))
-print("Data da ultima criação = ",os.path.getctime(filename))
-
 stats = os.stat(filename)
-print("ID do autor =",stats.st_uid)
+
+def time(data):
+    dt=datetime.datetime.fromtimestamp(data)
+    return dt.strftime("%Y-%m-%d %H:%M-%S")
+
+def get_file_attr(filename):
+    dicatrib ={
+    "Tamanho":os.path.getsize(filename),
+    "Data da ultima modificação":time(os.path.getmtime(filename)),
+    "Data da criação":time(os.path.getctime(filename)),
+    "ID do Autor":os.stat(filename).st_uid
+    }
+
+    return dicatrib
+print(get_file_attr(filename))
